f09c7d269a20ed9a2568cce4cf41dfc397311de7
Docky44 <simonpierre643@gmail.com>
2023-12-07 19:51:47 +0100

    Add cache + paquets + search pokemon by name

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from pokedex import views
 
 urlpatterns = [
-    path('', views.get_all_pokemon, name='index')
+   path('', views.get_all_pokemon_data, name='index'),
+   path('search/', views.search_pokemon, name='search_pokemon')
 ]
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,27 +1,118 @@
+from django.core.cache import cache
 import requests
 from django.shortcuts import render
+from requests.exceptions import RequestException
 from settings import *
+import time
 
-def get_all_pokemon(request):
 
-    print(API_URL)
+def fetch_pokemon_data(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except RequestException as e:
+        raise RequestException(f"Erreur lors de la connexion à l'API : {e}")
 
-    base_url = API_URL
 
-    all_pokemon_names = []
+def get_pokemon_info(url):
+    cached_pokemon_info = cache.get(url)
+    if cached_pokemon_info:
+        return cached_pokemon_info
 
-    next_page = base_url
-    while next_page:
-        response = requests.get(next_page)
+    pokemon_data = fetch_pokemon_data(url)
+    pokemon_info = {
+        'name': pokemon_data['name'],
+        'types': [t['type']['name'] for t in pokemon_data['types']]
+    }
 
-        if response.status_code == 200:
-            pokemon_data = response.json()
-            results = pokemon_data.get('results', [])
-            pokemon_names = [pokemon['name'] for pokemon in results]
-            all_pokemon_names.extend(pokemon_names)
-            next_page = pokemon_data.get('next')
+    cache.set(url, pokemon_info, timeout=3600)
+    return pokemon_info
+
+
+def get_pokemon_batch(url):
+    try:
+        data = fetch_pokemon_data(url)
+        pokemon_batch = []
+        for pokemon in data['results']:
+            pokemon_info = get_pokemon_info(pokemon['url'])
+            pokemon_batch.append(pokemon_info)
+            print(f"Récupération du Pokémon : {pokemon_info['name']}")
+
+        return pokemon_batch, data.get('next')
+    except RequestException as e:
+        raise RequestException(f"Erreur lors de la connexion à l'API : {e}")
+
+
+def get_all_pokemon(batch_size=100000):
+    try:
+        all_pokemon = []
+        offset = 0
+
+        while True:
+            gen_url = f"{API_URL}?limit={batch_size * 5}&offset={offset}"
+            data = fetch_pokemon_data(gen_url)
+
+            for pokemon in data['results']:
+                pokemon_info = get_pokemon_info(pokemon['url'])
+                all_pokemon.append(pokemon_info)
+                print(f"Récupération du Pokémon : {pokemon_info['name']}")
+
+            if not data.get('next'):
+                break
+            offset += batch_size * 5
+
+        return all_pokemon
+    except RequestException as e:
+        raise RequestException(f"Erreur lors de la connexion à l'API : {e}")
+
+
+def get_all_pokemon_data(request):
+    try:
+        start_time = time.time()
+
+        cached_pokemon = cache.get('all_pokemon_data')
+
+        if cached_pokemon:
+            return render(request, 'pokedex/index.html', {'pokemon_list': cached_pokemon})
         else:
-            error_message = "La requête à l'API PokeAPI a échoué."
-            return render(request, 'pokedex/errors.html', {'error_message': error_message})
+            start_fetch_time = time.time()
+            all_pokemon = get_all_pokemon()
+            end_fetch_time = time.time()
 
-    return render(request, 'pokedex/index.html', {'pokemon_names': all_pokemon_names})
+            cache.set('all_pokemon_data', all_pokemon, timeout=3600)
+            end_time = time.time()
+
+            fetch_duration = end_fetch_time - start_fetch_time
+            total_duration = end_time - start_time
+
+            print(f"Temps pour récupérer les données : {fetch_duration} secondes")
+            print(f"Temps total d'exécution : {total_duration} secondes")
+
+            return render(request, 'pokedex/index.html', {'pokemon_list': all_pokemon})
+    except Exception as e:
+        return render(request, 'pokedex/errors.html', {'error_message': f"Erreur : {e}"})
+
+
+def get_filtered_pokemon(query, all_pokemon):
+    return [pokemon for pokemon in all_pokemon if query.lower() in pokemon['name'].lower()]
+
+
+def search_pokemon(request):
+    try:
+        query = request.GET.get('search')
+        all_pokemon = cache.get('all_pokemon_data')
+
+        if query and all_pokemon:
+            cached_results = cache.get(query)
+            if cached_results:
+                filtered_pokemon = cached_results
+            else:
+                filtered_pokemon = get_filtered_pokemon(query, all_pokemon)
+                cache.set(query, filtered_pokemon, timeout=3600)
+
+            return render(request, 'pokedex/index.html', {'pokemon_list': filtered_pokemon, 'query': query})
+        else:
+            return render(request, 'pokedex/index.html', {'pokemon_list': all_pokemon})
+    except Exception as e:
+        return render(request, 'pokedex/errors.html', {'error_message': f"Erreur : {e}"})
