227ace723f569ec3749d603fea1d81d31bf9eaa2
Tom <tom.chanson@epsi.fr>
2023-12-17 17:16:42 +0100

    add team management

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -9,8 +9,12 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
     path('pokemon/user/', views.user_pokemon_list, name='user_pokemon_list'),
-    path('fight/team/', views.new_team, name='create_team'),
+    path('team/manage/', views.new_team, name='create_team'),
+    path('team/list/', views.team_list, name='team_list'),
+    path('team/manage/<int:team_id>/', views.update_team, name='update_team'),
+    path('team/delete/<int:team_id>/', views.delete_team, name='delete_team'),
     path('fight/history/', views.fight_history, name='fight_history'),
+
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- pokedex/views.py
+++ pokedex/views.py
@@ -83,24 +83,28 @@ def new_team(request):
     userProfil = UserProfile.objects.get(user=user)
     # get all fight of the user
     pokemons = userProfil.pokemon_set.all()
-    if user_in_fight(userProfil):
-        return redirect('fight_history')
     if request.method == "POST":
         team_name = request.POST['team_name']
         team = Team(name=team_name, user=userProfil)
         pokemon_list_id = []
+        team_valid = True
         for i in range(1, 7):
             pokemon_id = request.POST['pokemon_'+str(i)]
             if pokemon_id:
                 # check if user has this pokemon or selected pokemon is not already in the team
                 pokemon = Pokemon.objects.get(id=pokemon_id)
                 if pokemon.user == userProfil:
-                    if pokemon_id not in pokemon_list_id:
-                        pokemon_list_id.append(pokemon_id)
-                    else:
-                        return render(request, 'team/createTeam.html', {'error': 'Vous ne pouvez pas ajouter deux fois le même pokemon', 'pokemons': pokemons, 'team_name': team_name})
+                    try:
+                        if int(pokemon_id) not in pokemon_list_id:
+                            pokemon_list_id.append(int(pokemon_id))
+                    except ValueError:
+                        pass
                 else:
-                    return render(request, 'team/createTeam.html', {'error': 'Vous ne pouvez pas ajouter un pokemon qui ne vous appartient pas', 'pokemons': pokemons, 'team_name': team_name})
+                    team_valid = False
+        if len(pokemon_list_id) != 6:
+            return render(request, 'team/createTeam.html', {'error': 'Vous ne pouvez pas ajouter deux fois le même pokemon', 'pokemons': pokemons, 'team_name': team_name, 'pokemon_list_id': pokemon_list_id, 'url_action' : 'create_team'})
+        if not team_valid:
+            return render(request, 'team/createTeam.html', {'error': 'Vous ne pouvez pas ajouter un pokemon qui ne vous appartient pas', 'pokemons': pokemons, 'team_name': team_name, 'pokemon_list_id': pokemon_list_id, 'url_action' : 'create_team'})
         team.save()
         index = 1
         for pokemon_id in pokemon_list_id:
@@ -112,10 +116,34 @@ def new_team(request):
         # create fight
         fight = Fight(team1=team)
         fight.save()
-
-        return redirect('index')
+        request.session['success'] = 'Equipe créée'
+        return redirect('team_list')
     else:
-        return render(request, 'team/createTeam.html', {'pokemons': pokemons})
+        return render(request, 'team/createTeam.html', {'pokemons': pokemons, 'url_action' : 'create_team'})
+
+@login_required(login_url='login')
+def team_list(request):
+    user = request.user
+    userProfil = UserProfile.objects.get(user=user)
+    teams = userProfil.team_set.all()
+    list_team = []
+    for team in teams:
+        team_info = {
+            "name": team.name,
+            "id": team.id
+        }
+        team_pokemon = team.teampokemon_set.all()
+        for pokemon in team_pokemon:
+            team_info["pokemon_"+str(pokemon.order)] = {}
+            team_info["pokemon_"+str(pokemon.order)]["name"] = pokemon.pokemon.name
+            team_info["pokemon_"+str(pokemon.order)]["id"] = pokemon.pokemon.id
+        list_team.append(team_info)
+    print(list_team)
+    success = request.session.get('success', '')
+    error = request.session.get('error', '')
+    request.session['success'] = ''
+    request.session['error'] = ''
+    return render(request, 'team/listTeam.html', {'list_team': list_team, 'success': success, 'error': error})
 
 
 def user_in_fight(userProfil):
@@ -162,3 +190,68 @@ def fight_history(request):
     print(list_fight)
     return render(request, 'fight/history.html', {'list_fight': list_fight, 'in_fight': in_fight})
 
+@login_required(login_url='login')
+def delete_team(request, team_id):
+    if request.method == "POST":
+        team = Team.objects.get(id=team_id)
+        user = request.user
+        userProfil = UserProfile.objects.get(user=user)
+        teams = userProfil.team_set.all()
+        if team in teams:
+            team.delete()
+            request.session['success'] = 'Equipe supprimée'
+            return redirect('team_list')
+        else:
+            request.session['error'] = 'Vous ne pouvez pas supprimer cette équipe'
+            return redirect('team_list')
+    else:
+        return redirect('team_list')
+
+
+@login_required(login_url='login')
+def update_team(request, team_id):
+    team = Team.objects.get(id=team_id)
+    user = request.user
+    userProfil = UserProfile.objects.get(user=user)
+    teams = userProfil.team_set.all()
+    pokemons = userProfil.pokemon_set.all()
+    pokemon_list_id = []
+
+    # url_action = 'update_team' and the id of the team
+    url_action = 'update_team'
+    if team in teams:
+        if request.method == "POST":
+            team_name = request.POST['team_name']
+            team.name = team_name
+            team.save()
+            for i in range(1, 7):
+                pokemon_id = request.POST['pokemon_'+str(i)]
+                if pokemon_id:
+                    # check if user has this pokemon or selected pokemon is not already in the team
+                    pokemon = Pokemon.objects.get(id=pokemon_id)
+                    if pokemon.user == userProfil:
+                        if pokemon_id not in pokemon_list_id:
+                            pokemon_list_id.append(pokemon_id)
+                        else:
+                            return render(request, 'team/createTeam.html', {'error': 'Vous ne pouvez pas ajouter deux fois le même pokemon', 'pokemons': pokemons, 'team_name': team_name, 'url_action' : url_action, 'id_team': team_id})
+                    else:
+                        return render(request, 'team/createTeam.html', {'error': 'Vous ne pouvez pas ajouter un pokemon qui ne vous appartient pas', 'pokemons': pokemons, 'team_name': team_name, 'url_action' : url_action, 'id_team': team_id})
+            # delete all pokemon of the team
+            TeamPokemon.objects.filter(team=team).delete()
+            index = 1
+            for pokemon_id in pokemon_list_id:
+                # add pokemon to team
+                pokemon = Pokemon.objects.get(id=pokemon_id)
+                teamPokemon = TeamPokemon(team=team, pokemon=pokemon, order=index)
+                teamPokemon.save()
+                index += 1
+            return redirect('team_list')
+        else:
+            team_pokemon = team.teampokemon_set.all()
+            for pokemon in team_pokemon:
+                pokemon_list_id.append(pokemon.pokemon.id)
+            return render(request, 'team/createTeam.html', {'url_action' : url_action, 'id_team': team_id, 'pokemons': pokemons, 'team_name': team.name, 'pokemon_list_id': pokemon_list_id})
+    else:
+        request.session['error'] = 'Vous ne pouvez pas modifier cette équipe'
+        return redirect('team_list')
+    
